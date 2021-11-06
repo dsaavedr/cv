8e7eb18cfa46a551cacda5ada9a9cad49ec0bb5c
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-11-06 17:09:36 -0500

    Added rule for loading imported CSS

--- src/Component.js
+++ src/Component.js
@@ -1,7 +1,10 @@
+import "./style.css";
+
 export default function Component() {
     const element = document.createElement("div");
 
     element.innerHTML = "Hello Webpack";
+    element.classList.add("hello");
 
     return element;
 }
